d46f6adc24a670c8ed63a200340e4b6fe76efe4a
Matthew Plant <maplant95@gmail.com>
2018-08-10 12:03:27 -0700

    Customizations

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
   :after (lsp-mode))
 
 (setq-default c-default-style "k&r"
-              c-basic-offset 2
+              c-basic-offset 4
               tab-width 2
               indent-tabs-mode nil)
 
@@ -103,6 +103,8 @@
   (add-hook 'rust-mode-hook #'flycheck-mode)
   (add-hook 'rust-mode-hook #'lsp-rust-enable))
 
+(use-package auto-revert-mode
+  :mode ("\\.log\\'" . auto-revert-mode))  
 
 (use-package ggtags
   :ensure t)
@@ -206,7 +208,7 @@
          ("C-x C-f" . helm-find-files)
          ("C-x C-b" . helm-buffers-list)
          ("C-x b" . helm-buffers-list)
-         ("C-x g" . helm-grep-do-git-grep )
+         ("M-s g" . helm-grep-do-git-grep )
          ([f1] . helm-buffers-list))
   :config
   (helm-mode 1))
@@ -246,21 +248,23 @@
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; If you edit it by hand, you could mess it up, so be careul.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
+ '(c-basic-offset 4 t)
  '(column-number-mode t)
- '(custom-enabled-themes '(solarized-light))
+ '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes
-   '("5900bec889f57284356b8216a68580bfa6ece73a6767dfd60196e56d050619bc" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" "365d9553de0e0d658af60cff7b8f891ca185a2d7ba3fc6d29aadba69f5194c7f" "6f11ad991da959fa8de046f7f8271b22d3a97ee7b6eca62c81d5a917790a45d9" "611e38c2deae6dcda8c5ac9dd903a356c5de5b62477469133c89b2785eb7a14d" "4182c491b5cc235ba5f27d3c1804fc9f11f51bf56fb6d961f94788be034179ad" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))
+   (quote
+    ("5900bec889f57284356b8216a68580bfa6ece73a6767dfd60196e56d050619bc" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" "365d9553de0e0d658af60cff7b8f891ca185a2d7ba3fc6d29aadba69f5194c7f" "6f11ad991da959fa8de046f7f8271b22d3a97ee7b6eca62c81d5a917790a45d9" "611e38c2deae6dcda8c5ac9dd903a356c5de5b62477469133c89b2785eb7a14d" "4182c491b5cc235ba5f27d3c1804fc9f11f51bf56fb6d961f94788be034179ad" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(display-time-mode t)
  '(inhibit-startup-screen t)
  '(jdee-db-active-breakpoint-face-colors (cons "#0d0f11" "#7FC1CA"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#0d0f11" "#A8CE93"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#0d0f11" "#899BA6"))
- '(linum-format 'dynamic)
+ '(linum-format (quote dynamic))
  '(lsp-ui-doc-include-signature t)
  '(lsp-ui-doc-use-childframe t)
  '(lsp-ui-sideline-enable t)
@@ -270,12 +274,15 @@
  '(org-fontify-quote-and-verse-blocks t)
  '(org-fontify-whole-heading-line t)
  '(package-selected-packages
-   '(csharp-mode cmake-mode htmlize helm disaster use-package doom-themes telephone-line minimap multi-compile fill-column-indicator column-marker multiple-cursors eshell-prompt-extras nlinum rust-playground company-racer company flycheck-rust flymake-rust racer cargo haskell-mode solarized-theme rust-mode magit go-mode glsl-mode ggtags cider))
+   (quote
+    (yaml-mode docker-tramp mvn lsp-rust csharp-mode cmake-mode htmlize helm disaster use-package doom-themes telephone-line minimap multi-compile fill-column-indicator column-marker multiple-cursors eshell-prompt-extras nlinum rust-playground company-racer company flycheck-rust flymake-rust racer cargo haskell-mode solarized-theme rust-mode magit go-mode glsl-mode ggtags cider)))
+ '(ring-bell-function (quote ignore))
  '(rust-indent-where-clause nil)
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
- '(tramp-syntax 'default nil (tramp)))
+ '(tramp-syntax (quote default) nil (tramp))
+ '(visible-bell nil))
 
 ;(set-default-font "Anonymous Pro-12")
 (custom-set-faces
